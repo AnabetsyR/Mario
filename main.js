6cf3fe8126591965220d76eea1c05f5494a83f9f
ANABETSY RIVERO <anabetsy001@gmail.com>
2016-07-20 11:34:59 -0400

    added player frames and it updates direction with arrow keys

--- main.js
+++ main.js
@@ -4,21 +4,6 @@ var background= document.getElementById("background")
 c.width = window.innerWidth;
 c.height = window.innerHeight*0.9;
 
-var player={
-  x : 800,
-  y : 180,
-  velX : 0,
-  velY : 0,
-  sprite : document.getElementById("player"),
-  update: function(){
-    this.x= this.x + this.velX;
-    this.y= this.y + this.velY;
-  },
-  render: function(){
-    //ctx.fillRect(this.x, this.y, 300, 85);
-    ctx.drawImage(this.sprite, this.x, this.y);
-  }
-};
 
 function animate(){
   ctx.fillStyle = "#000000";
@@ -29,16 +14,3 @@ function animate(){
 }
 animate();
 
-$(document).keydown(function(e){
-  if(e.keyCode == 37) player.velX= -5;
-  if (e.keyCode == 38) player.velY=-5;
-  if (e.keyCode == 39) player.velX=5;
-  if(e.keyCode == 40) player.velY=5;                    
-});
-
-$(document).keyup(function(e){
-  if(e.keyCode == 37) player.velX= 0;
-  if (e.keyCode == 38) player.velY=0;
-  if (e.keyCode == 39) player.velX=0;
-  if(e.keyCode == 40) player.velY=0;                    
-});
